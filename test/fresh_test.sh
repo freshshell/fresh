e9ea6ca651eefbaaa2b90f5e91499671353aa583
Jason Weathered <jason@jasoncodes.com>
2012-10-08 08:34:23 +1000

    Build local files from ~/.freshrc.

--- test/fresh_test.sh
+++ test/fresh_test.sh
@@ -1,1 +1,39 @@
+it_concatenates_local_shell_files() {
+  echo fresh aliases/git >> $FRESH_RCFILE
+  echo fresh aliases/ruby >> $FRESH_RCFILE
+
+  mkdir -p $FRESH_LOCAL/aliases
+  echo "alias gs='git status'" >> $FRESH_LOCAL/aliases/git
+  echo "alias gl='git log'" >> $FRESH_LOCAL/aliases/git
+  echo "alias rake='bundle exec rake'" >> $FRESH_LOCAL/aliases/ruby
+
+  assertTrue 'returns true' bin/fresh
+
+  cat > tmp/sandbox/expected.sh <<EOF
+alias gs='git status'
+alias gl='git log'
+alias rake='bundle exec rake'
+EOF
+  diff -U2 tmp/sandbox/expected.sh $FRESH_PATH/build/shell.sh
+  assertTrue 'concatenates source files' $?
+}
+
+it_errors_with_missing_local_file() {
+  echo fresh no_such_file >> $FRESH_RCFILE
+  assertFalse 'returns false' bin/fresh
+}
+
+it_preserves_existing_compiled_file_when_failing() {
+  mkdir -p $FRESH_PATH/build
+  echo 'existing file' > $FRESH_PATH/build/shell.sh
+  cp $FRESH_PATH/build/shell.sh tmp/sandbox/ref_shell.sh
+
+  echo invalid >> $FRESH_RCFILE
+  assertFalse 'returns false' bin/fresh
+
+  assertTrue 'file exists' '[ -f "$FRESH_PATH/build/shell.sh" ]'
+  diff -U2 tmp/sandbox/ref_shell.sh $FRESH_PATH/build/shell.sh
+  assertTrue 'original content exists' $?
+}
+
 source test/test_helper.sh
