6343603c6f3ace2f6056374701e4cb6eafec2dbc
Jason Weathered <jason@jasoncodes.com>
2012-12-01 22:21:27 +1000

    Add tests for `fresh search`.

--- test/test_helper.sh
+++ test/test_helper.sh
@@ -75,4 +75,26 @@ EOF
   chmod +x $SANDBOX_PATH/bin/git
 }
 
+stubCurl() {
+  cat > $SANDBOX_PATH/bin/curl <<EOF
+#!/bin/bash -e
+echo curl >> $SANDBOX_PATH/curl.log
+for ARG in "\$@"; do
+  echo "\$ARG" >> $SANDBOX_PATH/curl.log
+done
+EOF
+if [[ "$1" == '--fail' ]]; then
+shift
+  cat >> $SANDBOX_PATH/bin/curl <<EOF
+echo "$*" >&2
+exit 1
+EOF
+else
+for LINE in "$@"; do
+  echo "echo \"$LINE\"" >> $SANDBOX_PATH/bin/curl
+done
+fi
+  chmod +x $SANDBOX_PATH/bin/curl
+}
+
 source test/support/shunit2
