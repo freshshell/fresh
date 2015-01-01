d3d96fbb65423b205810bcebd7321884dd7c3cee
Oliver Jan Krylow <okrylow@gmail.com>
2015-01-01 04:17:47 +0100

    Added missing ).

--- provision.sh
+++ provision.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 # Provisions an Arch box for testing fresh
 function __provision() {
+	# bring arch up-to-date and install fish
+	sudo pacman-db-upgrade
+	sudo pacman --noconfirm --noprogressbar --sync --sysupgrade --refresh --quiet fish
+	# make fish the default shell
+	sudo chsh -s /usr/bin/fish "$USER"
+	# install fresh from flasheater repo
 	FRESH_REPO='https://github.com/flasheater/fresh' /vagrant/install.sh
 	# source fresh output into fish init script
 	mkdir -p ~/.config/fish
 	echo 'source ~/.fresh/build/shell.sh' >> ~/.config/fish/config.fish
-	# bring arch up-to-date and install fish
-	sudo pacman --noconfirm --noprogressbar --sync --sysupgrade --refresh --quiet fish
-	# make fish the default shell
-	sudo chsh -s /usr/bin/fish "$USER"
 }
 __provision "$@"
