aaf1d20af56c96820621709e4412323f01079a90
Sascha Morgenstern <sascha.morgenstern@gmail.com>
2020-05-28 12:02:11 +0200

    Check .fresh-order file before call 'git show'
    git show returns with exit code 128, in case of non-existing file.
    
    This change checks if the .fresh-order is part of the directory
    and only if the file is present the git show command will be invoked.
    
    Issue: https://github.com/freshshell/fresh/issues/137

--- spec/fresh_spec.rb
+++ spec/fresh_spec.rb
@@ -623,8 +623,6 @@ describe 'fresh' do
           cd #{fresh_path + 'source/repo/name'}
           git ls-tree -r --name-only abc1237
           cd #{fresh_path + 'source/repo/name'}
-          git show abc1237:aliases/.fresh-order
-          cd #{fresh_path + 'source/repo/name'}
           git show abc1237:aliases/git.sh
           cd #{fresh_path + 'source/repo/name'}
           git show abc1237:aliases/ruby.sh
